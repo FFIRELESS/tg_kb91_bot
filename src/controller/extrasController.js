943ee24928bd56510cf72fcf1cdb578bbb3b67b0
dmytrotokarenko <dimatok006@gmail.com>
2023-02-26 17:47:00 +0200

    some improvements

--- src/controller/extrasController.js
+++ src/controller/extrasController.js
@@ -5,21 +5,18 @@ import * as path from "path";
 export const extrasController = {
     sendFeedback: async (msg) => {
         logger.info(`@${msg.chat?.username} id=${msg.chat?.id} requested sending feedback`)
-        const text = `[${new Date().toISOString()}] @${msg.chat?.username ? msg.chat.username : msg.chat.id}: `+
+        const text = `[${new Date().toISOString()}] @${msg.chat?.username ? msg.chat.username : msg.chat.id}: ` +
             `${msg.message.text.substring(10, msg.message.text.length)}\n`;
         const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-        try {
-            fs.appendFile(`${__dirname}/../../feedback.txt`, text, (err) => {
-                if (err) {
-                    logger.error('Failed writing feedback file')
-                }
-            });
-            await msg.reply(`\u{2705} Дякуємо за Ваш відгук!`)
-            logger.info('Success sending feedback');
-        } catch (error) {
-            await msg.reply('\u{2757}Помилка операції')
-            logger.error('Failed sending feedback');
-        }
+        fs.appendFile(`${__dirname}/../../feedback.txt`, text, async (err) => {
+            if (err) {
+                await msg.reply('\u{2757}Помилка операції')
+                logger.error('Failed sending feedback');
+            } else {
+                await msg.reply(`\u{2705} Дякуємо за Ваш відгук!`)
+                logger.info('Success sending feedback');
+            }
+        });
     }
 }
